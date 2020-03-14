55672497483af2fe5ad864c1393d18f66826bb3d
paraselenia <49591852+paraselenia@users.noreply.github.com>
2020-03-15 01:57:47 +0900

    Test

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "VRMKit",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v10_14)],
     products: [
         .library(name: "VRMKit", targets: ["VRMKit"]),
         .library(name: "VRMSceneKit", targets: ["VRMSceneKit"])
